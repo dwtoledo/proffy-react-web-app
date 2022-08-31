466c04a662d22f4acd48e35ce40747e57c1aba15
Douglas Toledo <dwtoledo@outlook.com>
2022-08-31 09:25:12 -0300

    Add Style for TeachersFilter

--- src/pages/TeachersList/components/TeachersFilter/index.tsx
+++ src/pages/TeachersList/components/TeachersFilter/index.tsx
@@ -1,6 +1,7 @@
 import { SubmitHandler, useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import teacherFiltersValidators from "./validators/teachers-filters-validators";
+import "./styles.css";
 
 interface IFilterFormInputs {
   teacher_subject: string;
@@ -30,17 +31,23 @@ function TeachersFilter({ filtersOutput }: TeachersFilterProps) {
   return (
     <form className="teachers-list__filters" onSubmit={handleSubmit(onSubmit)}>
       <div className="teachers-list__input">
-        <label htmlFor="teacher_subject">Subject</label>
+        <label htmlFor="teacher_subject">Subject:</label>
         <input
           type="text"
           id="teacher_subject"
           {...register("teacher_subject")}
         />
-        <p>{errors.teacher_subject?.message}</p>
+        <p
+          style={{
+            display: errors.teacher_subject?.message ? "block" : "none",
+          }}
+        >
+          {errors.teacher_subject?.message}
+        </p>
       </div>
 
       <div className="teachers-list__input">
-        <label htmlFor="class_week_days">Week day</label>
+        <label htmlFor="class_week_days">Week day:</label>
         <select id="class_week_days" {...register("class_week_days")}>
           <option value="all">All</option>
           <option value="monday">Monday</option>
@@ -54,11 +61,16 @@ function TeachersFilter({ filtersOutput }: TeachersFilterProps) {
       </div>
 
       <div className="teachers-list__input">
-        <label htmlFor="class_time">Time</label>
+        <label htmlFor="class_time">Time:</label>
         <input type="time" id="class_time" {...register("class_time")} />
       </div>
 
-      <button type="submit">Search</button>
+      <div className="teachers-list__input">
+        <label style={{ visibility: "hidden" }}>Search:</label>
+        <button type="submit" className="teachers-list__find-bttn">
+          Find
+        </button>
+      </div>
     </form>
   );
 }
--- src/pages/TeachersList/components/TeachersFilter/validators/teachers-filters-validators.tsx
+++ src/pages/TeachersList/components/TeachersFilter/validators/teachers-filters-validators.tsx
@@ -3,7 +3,7 @@ import * as yup from "yup";
 const teacherFiltersValidators = yup.object().shape({
   teacher_subject: yup
     .string()
-    .min(2, "Subject must have at least 2 characters"),
+    .min(2, "Subject must have at least 2 characters!"),
 });
 
 export default teacherFiltersValidators;
